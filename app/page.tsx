a1b0e5461c23bacfe81cba2658c5fbef81de3bb4
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-27 14:46:21 +0900

    Update page.tsx
    Deleting app heading

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Pokemon from "./pokemon/page";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1 className="text-2xl font-bold">Find your favourite pokemon here</h1>
       <Pokemon />
     </main>
   );
